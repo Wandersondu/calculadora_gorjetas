de5ded608f7e213f4f8e5b5c0a5c3792739bab53
Wanderson Dutra <74315664+Wandersondu@users.noreply.github.com>
2021-08-26 23:46:32 +0100

    add news file

---
+++ Curso Start na carreira/javascript.js
@@ -0,0 +1,29 @@
+function calculateTip(event) {
+    event.preventDefault()
+
+    let bill = document.getElementById('bill').Value
+    let serviceQuality = document.getElementById('serviceQuality').Value
+    let numOfPeople = document.getElementById('people').Value
+
+    if (bill == '' | serviceQuality == 0) {
+        alert("Favor da uma gorjeta mão de vaca!")
+        return
+    }
+
+    if (numOfPeople == "" | numOfPeople <= 1) {
+        numOfPeople = 1
+        document.getElementById('each').style.display = "none"
+    } else {
+        document.getElementById('each').style.display = "block"
+    }
+
+    let total = (bill * serviceQuality) / numOfPeople
+    total = total.toFixed(2)
+    document.getElementById('tip').innerHTML = total
+    document.getElementById('totalTip').style.display = "block"
+}
+
+document.getElementById('totalTip').style.display = "none"
+document.getElementById('each').style.display = "nome"
+
+document.getElementById('tipsForm').addEventListener('submit', calculateTip)
